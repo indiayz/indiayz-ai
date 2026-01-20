bada9b5d3eb7b02316f44fa3a8be28ee001005ee
ꜱᴀʙᴋᴀ ʙʜᴀɪ <deepanshumeena529@gmail.com>
2026-01-20 22:16:33 +0530

    Update __init__.py

--- indiayz/__init__.py
+++ indiayz/__init__.py
@@ -1,7 +1,7 @@
 from .wikipedia import search as wikipedia_search
 from .quote import random as random_quote
 from .media import download as media_download
-form .chat import chat
+from .chat import chat
 
 __all__ = [
     "wikipedia_search",
